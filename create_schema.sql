2e4be5a08391cb703e668311d5993257ad8c3f20
Tanvir23khan <tanvir23.khan@gmail.com>
2020-05-21 23:40:01 -0700

    fixed data loading

--- create_schema.sql
+++ create_schema.sql
@@ -11,7 +11,6 @@ CREATE TABLE price_data_df (
 	esg_rating int
 );
 
-
 drop table if exists dividend_df;
 CREATE TABLE dividend_df (
 	symbol TEXT PRIMARY KEY,
@@ -21,5 +20,13 @@ CREATE TABLE dividend_df (
 	pay_date date
 );
 
-
 select * from price_data_df;
+select * from dividend_df;
+
+-- Joins tables
+SELECT price_data_df.symbol, price_data_df.company, price_data_df.price, price_data_df.low, price_data_df.high,
+price_data_df.previous_close, price_data_df.esg_rating, dividend_df.symbol, dividend_df.dividend_yield, 
+dividend_df.annualized_dividend, dividend_df.ex_div_date, dividend_df.pay_date
+FROM price_data_df
+JOIN dividend_df
+ON price_data_df.symbol = dividend_df.symbol;
